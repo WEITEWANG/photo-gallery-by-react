0983cccc1d6a888873f3da87d2b8d6ba1a5d62ba
DESKTOP-SGSCI7V\Wangwei <wang.wei18@cgtn.com>
2018-07-24 17:36:35 +0800

    build basic constructure

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,17 +1,29 @@
 require('normalize.css/normalize.css');
-require('styles/App.css');
+require('styles/App.scss');
 
 import React from 'react';
-
-let yeomanImage = require('../images/yeoman.png');
+// 引入imageData数据
+var imagesData=require('../data/imgData.json');
+// 将imgData数据转换成可以引用的图片格式
+// 利用自执行函数将图片名信息转换为图片url信息
+imagesData=(function genImageUrl(imagesDataArray){
+    for(let i=0;i<imagesDataArray.length;i++){
+      var singleImageData=imagesDataArray[i];
+      console.log(singleImageData);
+      singleImageData.imageURL=require('../images/'+singleImageData.fileName);
+      imagesDataArray[i]=singleImageData;
+    }
+    return imagesDataArray;
+})(imagesData);
+// let yeomanImage = require('../images/yeoman.png');
 
 class AppComponent extends React.Component {
   render() {
     return (
-      <div className="index">
-        <img src={yeomanImage} alt="Yeoman Generator" />
-        <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
-      </div>
+      <section className="stage">
+          <section className="img-sec"></section>
+          <nav className="controller-nav"></nav>
+      </section>
     );
   }
 }
